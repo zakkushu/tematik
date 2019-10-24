bf185f5c6e6cd99a72d145c114940b1f79e30191
zakkushu <32445481+zakkushu@users.noreply.github.com>
2019-10-24 14:03:53 +0700

    Add files via upload

---
+++ tematik/guestlist/read.php
@@ -0,0 +1,49 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+include_once '../config/database.php';
+include_once '../objects/guestlist.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$guestlist = new guestlist($db);
+ 
+
+$stmt = $guestlist->read();
+$num = $stmt->rowCount();
+
+ 
+if($num>0){
+ 
+    $guestlist_arr=array();
+    $guestlist_arr["records"]=array();
+ 
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        
+        extract($row);
+ 
+        $guestlist_item=array(
+            "nama_e" => $nama_e,
+            "nama" => $nama,
+            "tname" => $tname,
+            "ID_K" => $ID_K
+        );
+ 
+        array_push($guestlist_arr["records"], $guestlist_item);
+    }
+ 
+    http_response_code(200);
+ 
+    echo json_encode($guestlist_arr);
+}
+ 
+else{
+ 
+    http_response_code(201);
+ 
+    echo json_encode(
+        array("message" => "No guestlist found.")
+    );
+}
